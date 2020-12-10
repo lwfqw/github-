e4a4fbe8e0e1ed08e09e8e71fbfbd2a519048380
lwf <519155720@qq.com>
2020-12-10 10:19:34 +0800

    忽略文件

--- consumer/src/main/java/com/lwf/first/coonfig/RestBean.java
+++ consumer/src/main/java/com/lwf/first/coonfig/RestBean.java
@@ -1,5 +1,6 @@
 package com.lwf.first.coonfig;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -16,6 +17,7 @@ import javax.annotation.Resource;
 @Configuration
 public class RestBean {
     @Bean
+    @LoadBalanced
     public RestTemplate getRest(){
         return new RestTemplate();
     }
